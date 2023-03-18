934a4c5059d914b33c10a67846e1e76ac43c2235
aisinker <aisinker@outlook.com>
2023-03-18 12:23:19 +0800

    update

--- src/config.py
+++ src/config.py
@@ -16,8 +16,7 @@ for s in server_ids:
     ALLOWED_SERVER_IDS.append(int(s))
 
 # Send Messages, Create Public Threads, Send Messages in Threads, Manage Messages, Manage Threads, Read Message History, Use Slash Command
-BOT_INVITE_URL = f"https://discord.com/api/oauth2/authorize?client_id={DISCORD_CLIENT_ID}&permissions=328565073920&scope=bot"
-
+BOT_INVITE_URL = f"https://discord.com/api/oauth2/authorize?client_id={DISCORD_CLIENT_ID}&permissions=534723951680&scope=bot%20applications.commands"
 SECONDS_DELAY_RECEIVING_MSG = (
     3  # give a delay for the bot to respond so it can catch multiple messages
 )
