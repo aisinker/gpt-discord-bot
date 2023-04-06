515c2b1215022f292336ab6a4a30da3c68715aca
aisinker <aisinker@outlook.com>
2023-04-06 23:30:39 +0800

    update

--- src/config.py
+++ src/config.py
@@ -7,13 +7,15 @@ load_dotenv()
 DISCORD_BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
 DISCORD_CLIENT_ID = os.environ["DISCORD_CLIENT_ID"]
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-HTTP_PROXY = os.environ["HTTP_PROXY"]
-DEBUG_LOG = True if os.environ["DEBUG_LOG"].lower() == "on" else False
 
 ALLOWED_SERVER_IDS: List[int] = []
 server_ids = os.environ["ALLOWED_SERVER_IDS"].split(",")
 for s in server_ids:
     ALLOWED_SERVER_IDS.append(int(s))
+
+HTTP_PROXY = os.environ.get("HTTP_PROXY")
+DEBUG_LOG = True if os.environ.get(
+    "DEBUG_LOG") != None and os.environ["DEBUG_LOG"].lower() == "on" else False
 
 # Send Messages, Create Public Threads, Send Messages in Threads, Manage Messages, Manage Threads, Read Message History, Use Slash Command
 BOT_INVITE_URL = f"https://discord.com/api/oauth2/authorize?client_id={DISCORD_CLIENT_ID}&permissions=534723951680&scope=bot%20applications.commands"
